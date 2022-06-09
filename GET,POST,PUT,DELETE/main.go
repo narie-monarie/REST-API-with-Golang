837f6147dc7b0515fc786ea5cb39f53b5acf762c
narie <monarijob@yahoo.com>
2022-06-09 13:27:00 +0300

    ello

--- GET,POST,PUT,DELETE/main.go
+++ GET,POST,PUT,DELETE/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,6 +31,15 @@ func deleteHello(ctx *gin.Context) {
 	})
 }
 
+func getProductById(ctx *gin.Context) {
+	id := c.Params("id") //string
+	idn, _ := strconv.ParseInt(id, 10, 0)
+
+	ctx.IndentedJSON(http.StatusOK, gin.H{
+		"id":   idn,
+		"name": "Product A",
+	})
+}
 func main() {
 	r := gin.Default()
 
@@ -40,12 +50,18 @@ func main() {
 	r.DELETE("/", deleteHello)
 
 	//grouping
-	r1 := r.Group("/api"){
+	var r1 = r.Group("/api")
+	{
 		r1.GET("/hello", getHello)
 		r1.POST("/hello", postHello)
 		r1.PUT("/hello", putHello)
 		r1.DELETE("/hello", deleteHello)
 	}
+
+	//handling path Params
+	r.GET("/product/:id", getProductById)
+	r.GET("/profile/:username", showProfile)
+	r.GET("/compute/:num1/add/:num2", compute)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
